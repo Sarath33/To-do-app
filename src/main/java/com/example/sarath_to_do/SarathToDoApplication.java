f39efc62a33d40b3d1fd8206254876dbc1a723a7
Sarath Gopal G <sarath@Saraths-MacBook-Pro.local>
2024-11-25 23:16:43 +0530

    Initial commit

---
+++ src/main/java/com/example/sarath_to_do/SarathToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarath_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarathToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarathToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sarath_to_do/SarathToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sarath_to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarathToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
